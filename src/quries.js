9a165c6cad884b2aa1d5aa1e03041c2281782aec
rohitdutt <parsairohit2@gmail.com>
2021-10-06 01:09:57 +0530

    ssl

--- src/quries.js
+++ src/quries.js
@@ -6,7 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: true
+  ssl: { 
+      rejectUnauthorized: false 
+    }
 });
 
 const hashids = new Hashids("Shittttttttttttttttt");
